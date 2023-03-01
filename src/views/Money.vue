c6eaf8c9ba262188b2d4d3170f546f70a7936609
Rocca <1142994228@qq.com>
2023-03-01 17:21:51 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">Money.vue</div>
     <Nav />
     </div>
 </template>
@@ -11,8 +12,21 @@ export default {
 name:"Money",
 components:{Nav}
 }
-</script>
+</script >
+<style lang="scss" scoped>
+.nav-wrapper{
+    border:1px solid green;
+    display:flex;
+    flex-direction: column;
+    height: 100vh;
+    // 如果设置min-height的话，content东西太多会被顶下来，因为是flex布局
+}
+.content{
+    flex-grow: 1;
+    // 全部占满，但是出现滚动条，是因为body默认有margin
+    overflow: auto;
+    // 如果东西太多，出现滚动条，但不影响nav
+}
 
-<style>
 
 </style>
