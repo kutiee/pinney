d1ed2b89693e973a8b6573295dcc3fbe7bdb7593
Rocca <1142994228@qq.com>
2023-03-02 16:46:32 +0800

    添加<Icon/>样式and路由激活

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,48 @@
 <template>
-     <div class="nav">
-    <router-link to="/money">
+    <nav>
+      <router-link to="/money" class="item" active-class="selected">
         <Icon name="money"/>
-        记账</router-link>
-    
-      <router-link to="/labels">
+        记账
+      </router-link>
+      <router-link to="/labels" class="item" active-class="selected">
         <Icon name="label"/>
-        标签</router-link> 
-    
-        <router-link to="/statistics">
-            <Icon name="statistics"/>
-            统计</router-link>
-     
-  </div>
-</template>
-
-<script lang="ts">
-
+        标签
+      </router-link>
+      <router-link to="/statistics" class="item" active-class="selected">
+        <Icon name="statistics"/>
+        统计
+      </router-link>
+    </nav>
+  </template>
+  
+  <script lang="ts">
+  
     export default {
-        name:"Nav"
+      name: 'Nav'
+    };
+  </script>
+  
+  <style lang="scss" scoped>
+    nav {
+      display: flex;
+      box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+      flex-direction: row;
+      font-size: 12px;
+      > .item {
+        padding: 2px 0;
+        width: 33.33333%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        .icon {
+          width: 32px;
+          height: 32px;
+        }
+      }
+      > .item.selected{
+        color: red;
+      }
     }
-</script>
-
-<style lang="scss" scoped>
-.nav{border:1px solid red;}
-</style> scoped>
-
-</style>
+  </style>
+  
