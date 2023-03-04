e07dac0a9d6395adadb8c52627e91fdb709bac54
Rocca <1142994228@qq.com>
2023-03-04 19:24:47 +0800

    将tag的string改为{id,name}

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import { component } from 'vue/types/umd';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
+import EditLabel from '@/views/EditLabel.vue';
 Vue.use(VueRouter)
 const routes = [
   {
@@ -23,6 +24,10 @@ const routes = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path: '/labels/edit',
+    component: EditLabel
   },
   {
     path: "*",
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,18 @@
+<template>
+   <Layout>
+    编辑标签
+   </Layout>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+import Layout from '@/components/Layout.vue';
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
